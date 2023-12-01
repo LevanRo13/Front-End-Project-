0f78bcecf06b6b3a61359fe6ea94cfa904b1939d
LevanRo13 <mirandaval485@hotmail.com>
2023-11-30 23:07:24 -0300

    Navbar actualization

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,43 +4,41 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.css'
 
 const Header = () => {
-    return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+  return (
+    <Navbar expand="lg" >
       <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+      <NavDropdown title={
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+      </svg>
+        } id="navbarScrollingDropdown" className='me-5'>
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+            </NavDropdown>
+        <Navbar.Brand href="#">
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <Nav.Link href="#action1" className='me-5'>Logo</Nav.Link>
+            <Nav.Link href="#action2" className='me-2'>Categorias</Nav.Link>
+            <Nav.Link href="#action3" className='me-2'>Productos</Nav.Link>
+            <Nav.Link href="#action3" className='me-2'>Conocenos</Nav.Link>
+
+
           </Nav>
-          <Form className="d-flex">
+          <Form className="me-auto custom-form">
             <Form.Control
               type="search"
-              placeholder="Search"
-              className="me-2"
+              placeholder="¿Qué estás buscando?"  
+              className="me-2 form-control-sm"
               aria-label="Search"
             />
-            <Button variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
