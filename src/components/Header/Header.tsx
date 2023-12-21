9df5f5054b833eccdb4e7b39917a872b7fea957a
ivannmolina <ivan14molina@gmail.com>
2023-12-21 17:14:52 -0300

    /commit/ gente me regalaron un header casi dañao

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,63 +1,87 @@
-  import React from 'react';
-  import Navbar from 'react-bootstrap/Navbar';
-  import Container from 'react-bootstrap/Container';
-  import Nav from 'react-bootstrap/Nav';
-  import Form from 'react-bootstrap/Form';
-  import NavDropdown from 'react-bootstrap/NavDropdown';
-  import { FaSearch, FaUserCircle } from 'react-icons/fa';
-  import { useNavigate } from 'react-router-dom';
-  import Logo from '../Logo/Logo';
-  import './Header.css';
-  import { Col, Row } from 'react-bootstrap';
+    import React, { useState } from 'react';
+    import Navbar from 'react-bootstrap/Navbar';
+    import Container from 'react-bootstrap/Container';
+    import Nav from 'react-bootstrap/Nav';
+    import Form from 'react-bootstrap/Form';
+    import NavDropdown from 'react-bootstrap/NavDropdown';
+    import { FaCartPlus, FaHeart, FaMapMarker, FaSearch, FaStore, FaUserCircle } from 'react-icons/fa';
+    import { useNavigate } from 'react-router-dom';
+    import Logo from '../Logo/Logo';
+    import './Header.css';
+    import { Col, Row } from 'react-bootstrap';
+import { Button } from 'react-bootstrap/lib/InputGroup';
 
-  function Header() {
-    const navigate = useNavigate();
+    function Header() {
+      const [expanded, setExpanded] = useState(false);
+      const navigate = useNavigate();
+    
+      const handleToggle = () => {
+        setExpanded(!expanded);
+      };
+    
+      return (
+        <Navbar expand="lg" className="custom-navbar">
+          <Container fluid className='container-nav'>
+    
+            {/* Custom Navbar.Toggle with Logo */}
+            <Navbar.Toggle onClick={handleToggle} aria-controls="navbarScroll" className="custom-toggle">
+             <Logo />
+            </Navbar.Toggle>
+    
+            <Navbar.Collapse in={expanded}>
 
-    return (
-      <Navbar expand="lg" className="custom-navbar">
-        <Container fluid className='container-nav'>
-          <Navbar.Brand onClick={() => navigate('/')}><Logo /></Navbar.Brand>
+                {/* Navbar.Brand with Logo (conditionally rendered based on expanded state) */}
+                <Navbar.Brand onClick={() => navigate('/')} className={`d-lg-block ${expanded ? 'd-none' : ''}`}>
+              <Logo />
+            </Navbar.Brand>
+              <Nav className="custom-nav">
 
-          <Nav className="custom-nav">
-
-            <Container className='custom-containernavlink'>
-              <Nav.Link className="custom-navlink" onClick={() => navigate('/categorias')}>Categorias</Nav.Link>
-              <Nav.Link className="custom-navlink" href="#productos">Productos</Nav.Link>
-              <Nav.Link className="custom-navlink" href="#conocenos">Conócenos</Nav.Link>
-            </Container>
+              <Container className='custom-containernavlink'>
+                <Nav.Link className="custom-navlink" onClick={() => navigate('/categorias')}>Categorias</Nav.Link>
+                <Nav.Link className="custom-navlink" href="#productos">Productos</Nav.Link>
+                <Nav.Link className="custom-navlink" href="#conocenos">Conócenos</Nav.Link>
+              </Container>
+              
             
-          
 
-          <Form className="custom-form">
-              <Row className="align-items-center">
-                <Col xs={8}>
-                  <Form.Control
-                    type="search"
-                    placeholder="¿Qué estás buscando?"
-                    className="custom-input"
-                    aria-label="Search"
-                  />
-                </Col>
-                  <Col xs={2} className="text-end">
-                  <FaSearch className="search-icon" />
-                </Col>
-              </Row>
-            </Form>
-            </Nav>
-          <NavDropdown
-            title={<FaUserCircle className="bi bi-person-circle iconUser" />}
-            id="navbarUser"
-            className='customNavbarUser'>
-            <NavDropdown.Item href="#ingresar">Ingresá</NavDropdown.Item>
-            <NavDropdown.Item href="#registrate">Regístrate</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#cuenta">Ingresa a tu cuenta</NavDropdown.Item>
-          </NavDropdown>
+            <Form className="custom-form">
+                <Row className="align-items-center">
+                  <Col xs={8}>
+                    <Form.Control
+                      type="search"
+                      placeholder="¿Qué estás buscando?"
+                      className="custom-input"
+                      aria-label="Search"
+                    />
+                  </Col>
+                    <Col xs={2} className="text-end">
+                    <FaSearch className="search-icon" />
+                  </Col>
+                </Row>
+              </Form>
+              </Nav>
+
+              <div className="ml-auto customNavbarUser">
+                  <div className="icon-container">
+                    <FaUserCircle className="bi bi-person-circle iconUser" />
+                    <span className="icon-text">Usuario</span>
+                  </div>
+                  <div className="icon-container">
+                    <FaHeart />
+                    <span className="icon-text">Favoritos</span>
+                  </div>
+                  <div className="icon-container">
+                    <FaCartPlus />
+                    <span className="icon-text">Carrito</span>
+                  </div>
+                </div>
+
+             
             
-          
-        </Container>
-      </Navbar>
-    );
-  }
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }
 
-  export default Header;
+    export default Header;
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,18 +1,23 @@
+// Logo.tsx
 import React from 'react';
-import './Logo.css';  // Asegúrate de tener el archivo CSS correspondiente
+import './Logo.css';
 
-const Logo: React.FC = () => {
+interface LogoProps {
+  className?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
-    <div className="logo-container">
-        <div className="lines-container">
-            <div className="line cyan"></div>
-            <div className="line yellow"></div>
-            <div className="line cyan"></div>
-        </div>
-        <div className="text-container">
-            <div className="text">SMART</div>
-            <div className="text">ARGENTINA</div>
-        </div>
+    <div className={`logo-container ${className || ''}`}>
+      <div className="lines-container">
+        <div className="line cyan"></div>
+        <div className="line yellow"></div>
+        <div className="line1 cyan"></div>
+      </div>
+      <div className="text-container">
+        <div className="text">SMART</div>
+        <div className="text">ARGENTINA</div>
+      </div>
     </div>
   );
 }
